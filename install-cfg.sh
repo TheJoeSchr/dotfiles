d060c68f140dc294816e667f3b741489d7820581
Joe Schr <joe.vandelay@gmail.com>
2020-03-21 14:27:04 +0100

    Adds install script which can be called on new machine to make config() etc

---
+++ install-cfg.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# call with 
+# curl -Lks https://bitbucket.org/JozephS/dotfiles/raw/master/install-cfg.sh | /bin/bash
+
+git clone --bare git clone git@bitbucket.org:JozephS/dotfiles.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
