1c8b35187470e7a7dc6b2e6ea62c14fc3baf5359
Joe Schr <joe.vandelay@gmail.com>
2020-03-29 14:52:11 +0200

    Adds neomutt

--- riceSystem.sh
+++ riceSystem.sh
@@ -15,7 +15,7 @@ sudo apt update
 sudo galliumos-update
 
 # install ALL THE THINGS
-sudo apt install -y neovim git nitrogen python3 python3-pip dmenu acpi xsel htop xbacklight ssh-askpass lazygit lxappearance maim xcompmgr unclutter
+sudo apt install -y neovim git nitrogen python3 python3-pip dmenu acpi xsel htop xbacklight ssh-askpass lazygit lxappearance maim xcompmgr unclutter neomutt urlview notmuch
 # fonts
 sudo apt install -y fonts-symbola fonts-noto-hinted fonts-powerline fonts-inconsolata
 
@@ -23,7 +23,7 @@ sudo apt install -y fonts-symbola fonts-noto-hinted fonts-powerline fonts-incons
 sudo apt install -y debian-keyring g++-multilib g++-7-multilib gcc-7-doc libstdc++6-7-dbg gcc-multilib autoconf automake libtool flex bison gdb gcc-doc  lighttpd  libstdc++-7-doc ctags vim-scripts python-greenlet-doc python-greenlet-dev python-greenlet-dbg python-crypto-doc python3-greenlet-dbg libkf5wallet-bin python-secretstorage-doc python-setuptools-doc dwm stterm surf
 
 # buildtools & dependencies
-sudo apt install -y xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev gcc make cmake pkg-config glib-2.0 autoconf automake pkg-config libncurses5-dev libncursesw5-dev bison cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev libxcb-ewmh2 libx11-dev libxft-dev fontconfig
+sudo apt install -y xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev gcc make cmake pkg-config glib-2.0 autoconf automake pkg-config libncurses5-dev libncursesw5-dev bison cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev libxcb-ewmh2 libx11-dev libxft-dev fontconfig isync msmtp pass
 
 # TODO download .ssh/ folder
 chmod 600 .ssh/*
@@ -129,6 +129,13 @@ cd ~/Downloads
 
 # mpd with google music
 pip3 install --user mopidy
+
+# muttwizard
+cd Downloads/
+git clone https://github.com/LukeSmithxyz/mutt-wizard
+cd mutt-wizard
+sudo make install
+cd Downloads/
 
 # pywal
 pip3 install --user pywal
