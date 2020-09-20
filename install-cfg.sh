b7c7e07fcc82b95a96b240919e23727407f343e0
Joe Schr <joe.vandelay@gmail.com>
2020-09-20 12:38:47 +0200

    [install-cfg.sh] try to fix mv not creating subdirs and fixes alias config
    by not using hardcoded git path

--- install-cfg.sh
+++ install-cfg.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-# call with 
+# call with
 # curl -Lks https://bitbucket.org/JozephS/dotfiles/raw/master/install-cfg.sh | /bin/bash
 
 git clone --bare https://bitbucket.org/JozephS/dotfiles.git $HOME/.cfg
-alias config="/usr/sbin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME" 
+
+alias config="$(which git) --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 
 mkdir -p .config-backup
 config checkout
@@ -12,7 +13,11 @@ if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} rsync -av --remove-source-files {} .config-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
+
+touch ~/.vimrc.local
+touch ~/.bashrc.local
+mkdir ~/Downloads
