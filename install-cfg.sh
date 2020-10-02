891f63fb41c69a1e82dd8c32f0bed015fdde80a7
Joe Schr <joe.vandelay@gmail.com>
2020-10-02 17:37:05 +0200

    [install] fix alias and home path

--- install-cfg.sh
+++ install-cfg.sh
@@ -5,19 +5,20 @@
 
 git clone --bare https://bitbucket.org/JozephS/dotfiles.git $HOME/.cfg
 
-alias config="$(which git) --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+config="$(which git) --git-dir=~~/.cfg/ --work-tree=~"
 
-mkdir -p .config-backup
-config checkout
+mkdir -p ~/.config-backup
+$config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} rsync -av --remove-source-files {} .config-backup/{}
+    $config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} rsync -av --remove-source-files {} .config-backup/{}
 fi;
-config checkout
-config config status.showUntrackedFiles no
+$config checkout
+$config config status.showUntrackedFiles no
 
 touch ~/.vimrc.local
 touch ~/.bashrc.local
 mkdir ~/Downloads
+source .bashrc
