a7577dd3bfc8c719af4c394d7d48b0bac38f16a6
Joe Schr <joe.vandelay@gmail.com>
2020-10-02 20:06:05 +0200

    login/xprofile/kde/plasma: fix on lxrush

--- .local/bin/kde-bspwm.sh
+++ .local/bin/kde-bspwm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export KDEWM=bspwm
-startkde
+command -v startkde >/dev/null 2>&1  >/dev/null 2>&1 && startkde
+command -v startplasma-x11 >/dev/null 2>&1  >/dev/null 2>&1 && startplasma-x11
 
 # /usr/bin/startplasma-x11
