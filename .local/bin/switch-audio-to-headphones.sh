c7a8a38d620d07e1c9380ccd423421215978be04
Joe Schr <joe.vandelay@gmail.com>
2020-11-29 16:09:46 +0100

    audio: add switch scripts, improve pulse config

---
+++ .local/bin/switch-audio-to-headphones.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+amixer -c 0 sset 'Stereo Upmixing' Front
+amixer -c 0 sset 'Front Panel' on
---
+++ .local/bin/switch-audio-to-surround.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+amixer -c 0 sset 'Stereo Upmixing' Front+Surround+Center/LFE+Back
+amixer -c 0 sset 'Front Panel' off
