bc68eba739d1c39c3103874be0715cbb7ea1ef98
Joe Schr <joe.vandelay@gmail.com>
2020-12-02 14:47:55 +0100

    fish: fish hardcoded home directory

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function config --wraps='git --git-dir=/home/joe/.cfg/ --work-tree=/home/joe' --description 'alias config=git  --git-dir=/home/joe/.cfg/ --work-tree=/home/joe'
-  git --git-dir=/home/joe/.cfg/ --work-tree=/home/joe $argv;
+function config --wraps='git --git-dir=$HOME/.cfg/ --work-tree=$HOME/gc' --description 'alias config=git  --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+  git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv;
 end
