aab97fdd0df6a5414f1ff10a09cf0c0b0ff32886
Joe Schr <joe.vandelay@gmail.com>
2020-12-20 22:19:07 +0100

    fish: add 1password helper `op-grep`

---
+++ .config/fish/functions/op-grep.fish
@@ -0,0 +1,5 @@
+# Defined in - @ line 1
+function op-pw-grep --description alias\ op-pw-grep="op list items | grep \$1 | show field \$2"
+  set -q argv[2]; or set argv[2] "password"
+  op list items | jq ".[] | select (.overview.title | contains (\"$argv[1]\")) | .uuid" | xargs -I % sh -c "op get item % --fields $argv[2]; echo \"\"";
+end
