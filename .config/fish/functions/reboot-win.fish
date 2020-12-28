35a5235b47491e0e50ef9c7a9b5afa5403a890ca
Joe Schr <joe.vandelay@gmail.com>
2020-12-28 18:22:04 +0100

    fish: reboot-win now reboots

--- .config/fish/functions/reboot-win.fish
+++ .config/fish/functions/reboot-win.fish
@@ -1,4 +1,5 @@
 # Defined in - @ line 1
 function reboot-win --wraps='sudo grub-reboot "Windows Boot Manager (on /dev/sdb1)"' --description 'alias reboot-win=sudo grub-reboot "Windows Boot Manager (on /dev/sdb1)"'
   sudo grub-reboot "Windows Boot Manager (on /dev/sdb1)";
+  sudo systemctl reboot;
 end
