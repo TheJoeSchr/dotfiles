1ad97f223e5b78fec015d0b9b67c09c327093763
Joe Schr <joe.vandelay@gmail.com>
2021-01-06 00:27:33 +0100

    fix: fish prompt, install yimmy

--- .config/fish/functions/fish_mode_prompt.fish
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -1,29 +1,28 @@
 function fish_mode_prompt --description 'Displays the current mode'
-  # # print new line before everything
+  # print new line before everything
   # set_color $fish_color_operator
   # echo " "
   # printf "\f\r"
 
-  # # Do nothing if not in vi mode
-  # if test "$fish_key_bindings" = "fish_vi_key_bindings"
-  #   switch $fish_bind_mode
-  #     case default
-  #       set_color --bold red
-  #       echo 🅽
-  #     case insert
-  #       # set_color --bold green
-  #       # echo 🅸
-  #       # set_color --bold green
-  #       set_color $fish_color_operator
-  #       echo "|"
-  #     case replace_one
-  #       set_color --bold green
-  #       echo 🆁
-  #     case visual
-  #       set_color --bold brmagenta
-  #       echo 🆅
-  #   end
-  #   set_color normal
-  #   printf " "
-  # end
+  # Do nothing if not in vi mode
+  if test "$fish_key_bindings" = "fish_vi_key_bindings"
+    switch $fish_bind_mode
+      case default
+        set_color --bold red
+        echo 🅽
+      case insert
+        # set_color --bold green
+        # echo 🅸
+        # set_color --bold green
+        set_color $fish_color_operator
+        echo ""
+      case replace_one
+        set_color --bold green
+        echo 🆁
+      case visual
+        set_color --bold brmagenta
+        echo 🆅
+    end
+    set_color normal
+  end
 end
