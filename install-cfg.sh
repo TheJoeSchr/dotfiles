1eb26d629232e78c0dbb860da876caab94bd6d78
JoeSchr <JoeSchr@users.noreply.github.com>
2021-01-07 19:17:57 +0100

    Update install-cfg.sh
    add --relative to create subdirs

--- install-cfg.sh
+++ install-cfg.sh
@@ -7,13 +7,13 @@ git clone --bare https://github.com/JoeSchr/dotfiles.git $HOME/.cfg
 
 config="$(which git) --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 
-mkdir -p $HOME/.backup-cfg/.config
+mkdir -p $HOME/.backup-cfg/
 $config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    $config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} rsync -av --remove-source-files {} .backup-cfg/{}
+    $config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} rsync -av --relative --remove-source-files {} .backup-cfg/{}
 fi;
 $config checkout
 $config config status.showUntrackedFiles no
