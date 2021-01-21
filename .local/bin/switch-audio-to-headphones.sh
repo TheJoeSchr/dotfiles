ca540a243df475ba259945c535829e6d0869d74b
Joe Schr <joe.vandelay@gmail.com>
2021-01-21 13:39:05 +0100

    ~/.local/bin/switch-audio: HP to 10% on switch

--- .local/bin/switch-audio-to-headphones.sh
+++ .local/bin/switch-audio-to-headphones.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 amixer -c DX sset 'Stereo Upmixing' Front
 amixer -c DX sset 'Front Panel' on
-# silecne other channels
-# amixer -c DX sset Master 0%
+# silence other channels
+amixer -c DX sset Master 0%
 # activate only front
-amixer -c DX sset Master 100%,100%,0%,0%,0%,0%,0%,0%
+amixer -c DX sset Master 10%,10%,0%,0%,0%,0%,0%,0%
