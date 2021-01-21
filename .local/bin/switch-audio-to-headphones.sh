8b761c98eacc5c2dba48418b626a5c6f5b1a946f
Joe Schr <joe.vandelay@gmail.com>
2021-01-21 13:34:05 +0100

    ~/.local/bin: adapt switch-audio and add: protontricks, youtube-dl

--- .local/bin/switch-audio-to-headphones.sh
+++ .local/bin/switch-audio-to-headphones.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 amixer -c DX sset 'Stereo Upmixing' Front
 amixer -c DX sset 'Front Panel' on
-amixer -c DX sset Master 100%
+# silecne other channels
+# amixer -c DX sset Master 0%
+# activate only front
+amixer -c DX sset Master 100%,100%,0%,0%,0%,0%,0%,0%
--- .local/bin/switch-audio-to-surround.sh
+++ .local/bin/switch-audio-to-surround.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-amixer -c DX sset Master 50%
+amixer -c DX sset Master 20%
 amixer -c DX sset 'Stereo Upmixing' Front+Surround+Center/LFE+Back
 amixer -c DX sset 'Front Panel' off
