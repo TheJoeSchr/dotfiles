58608e83c98699341b3690957b7f95d7b2b7a066
Joe Schr <joe.vandelay@gmail.com>
2021-01-22 12:07:40 +0100

    audio: fine tune surround volume balances for current room setup

--- .local/bin/switch-audio-to-surround.sh
+++ .local/bin/switch-audio-to-surround.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-amixer -c DX sset Master 20%
+amixer -c DX sset Master 20%,15%,20%,12%,25%,25%,0%,0%
 amixer -c DX sset 'Stereo Upmixing' Front+Surround+Center/LFE+Back
 amixer -c DX sset 'Front Panel' off
