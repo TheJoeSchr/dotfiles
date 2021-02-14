e8643b16303fd9ebad5a110721027b5151abe224
Joe Schr <joe.vandelay@gmail.com>
2021-02-14 17:49:25 +0100

    awiesomeeasybb: change timefrae

--- .local/bin/switch-audio-to-headphones.sh
+++ .local/bin/switch-audio-to-headphones.sh
@@ -4,4 +4,4 @@ amixer -c DX sset 'Front Panel' on
 # silence other channels
 amixer -c DX sset Master 0%
 # activate only front
-amixer -c DX sset Master 10%,10%,-10%,0%,0%,0%,0%,0%
+amixer -c DX sset Master 10%,10%,0%,0%,0%,0%,0%,0%
