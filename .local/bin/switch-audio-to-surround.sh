d9fafba30c327241c2ef82d34ca46b263733e7e9
Joe Schr <joe.vandelay@gmail.com>
2021-03-20 12:43:36 +0100

    re-balance surround audio channel

--- .local/bin/switch-audio-to-surround.sh
+++ .local/bin/switch-audio-to-surround.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-amixer -c DX sset Master 45%,45%,15%,15%,40%,50%,0,0
+amixer -c DX sset Master 45%,45%,15%,15%,40%,40%,0,0
 amixer -c DX sset 'Stereo Upmixing' Front+Surround+Center/LFE+Back
 amixer -c DX sset 'Front Panel' off
