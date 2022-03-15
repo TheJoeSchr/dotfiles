9d6a8c3c80052ae211e3ebf3ec14a063ed9e8720
Joe Schr <joe.vandelay@gmail.com>
2022-03-15 14:02:10 +0100

    fish: improve duck{sSA}

--- .config/fish/functions/duckS.fish
+++ .config/fish/functions/duckS.fish
@@ -1,4 +1,5 @@
 # Defined in - @ line 1
 function duckS --wraps='du -ckSh * | sort -rh | head' --description 'alias duckS=du -ckSh * | sort -rh | head'
-  du -ckSh * | sort -rh | head $argv;
+  set du (which du)
+  $du -ckSh * | sort -rh | head $argv;
 end
