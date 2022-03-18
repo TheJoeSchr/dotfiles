aed5ce3cb53fdf7475a1e2caec5bbc1eda05543c
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-03-18 22:48:45 +0100

    fish: update chrome-remote-desktop-patch

--- .config/fish/functions/chrome-remote-desktop-patch.fish
+++ .config/fish/functions/chrome-remote-desktop-patch.fish
@@ -1,5 +1,9 @@
 function chrome-remote-desktop-patch
   cp /opt/google/chrome-remote-desktop/chrome-remote-desktop ~/Downloads 
-  patch -i ~/Downloads/chrome-remote-desktop--use_existing_session.patch /opt/google/chrome-remote-desktop/chrome-remote-desktop -o ~/Downloads/chrome-remote-desktop.patched
-  sudo cp ~/Downloads/chrome-remote-desktop.patched /opt/google/chrome-remote-desktop/chrome-remote-desktop
+  patch ~/Downloads/chrome-remote-desktop -i ~/Downloads/chrome-remote-desktop--use_existing_session.patch  -o ~/Downloads/chrome-remote-desktop.patched
+  # patch sudo & existing session
+  # disabled because not sure it's working
+  # patch ~/Downloads/chrome-remote-desktop -i ~/Downloads/chrome-remote-desktop--use_existing_session.patch  -i ~/Downloads/chrome-remote-desktop--use_sudo.patch -o ~/Downloads/chrome-remote-desktop.patched
+  sudo mv ~/Downloads/chrome-remote-desktop.patched /opt/google/chrome-remote-desktop/chrome-remote-desktop
+  rm ~/Downloads/chrome-remote-desktop
 end
