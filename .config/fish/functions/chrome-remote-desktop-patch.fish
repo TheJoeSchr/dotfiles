91cbc09b6d4d0a14e2f4d9b5e00f37df879edc6e
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-03-18 23:07:50 +0100

    fish: crd, add chmod +x

--- .config/fish/functions/chrome-remote-desktop-patch.fish
+++ .config/fish/functions/chrome-remote-desktop-patch.fish
@@ -4,6 +4,7 @@ function chrome-remote-desktop-patch
   # patch sudo & existing session
   # disabled because not sure it's working
   # patch ~/Downloads/chrome-remote-desktop -i ~/Downloads/chrome-remote-desktop--use_existing_session.patch  -i ~/Downloads/chrome-remote-desktop--use_sudo.patch -o ~/Downloads/chrome-remote-desktop.patched
+  chmod +x ~/Downloads/chrome-remote-desktop.patched
   sudo mv ~/Downloads/chrome-remote-desktop.patched /opt/google/chrome-remote-desktop/chrome-remote-desktop
   rm ~/Downloads/chrome-remote-desktop
 end
