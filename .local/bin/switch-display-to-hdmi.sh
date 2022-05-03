b34bc393f89eefa6b0974d9b088996a23136097a
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-05-03 20:45:01 +0200

    update kwin, tmux, display scripts

--- .local/bin/switch-display-to-hdmi.sh
+++ .local/bin/switch-display-to-hdmi.sh
@@ -1,2 +1,5 @@
-xrandr --output HDMI-0 --mode 1920x1080 --above DP-4 --primary
-xrandr --output DP-4 --off
+# nvida hdmi
+xrandr --output HDMI-0 --mode 1920x1080 --rate 60
+# intel hdmi
+xrandr --output HDMI-1-2 --mode 1920x1080 --rate 60 --primary
+# xrandr --output DP-4 --off
