cdb99551d6daf859f1d93a7be08958c6361b6bb5
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-05-16 11:40:22 +0200

    ssh: add rechenknecht with proxy jump

--- .config/fish/functions/sshuttle-home.fish
+++ .config/fish/functions/sshuttle-home.fish
@@ -1,3 +1,3 @@
 function sshuttle-home --wraps='sshuttle -vDNHr ' --description 'alias sshuttle-home sshuttle -vDNHr routerwien.ddns.net -x routerwien.ddns.net:2222'
-  sshuttle -vDNHr routerwien.ddns.net -x routerwien.ddns.net:2222 $argv; 
+  sshuttle -vDNHr routerwien.ddns.net -x routerwien.ddns.net:22 -x routerwien.ddns.net:2222 $argv;
 end
