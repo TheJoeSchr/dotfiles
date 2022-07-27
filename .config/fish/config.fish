3ec6c5f4bbbc9382f275b5085ee9292d3cb5645b
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-07-27 20:16:24 +0200

    fish/nnn: add pistol config, ctrl-o fixed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,9 @@ set -gx EDITOR nvim
 set -gx VISUAL nvr
 set -gx BROWSER google-chrome-stable
 set -x NNN_OPENER ~/.config/nnn/plugins/nuke 
+
 set -x NNN_PLUG 'f:finder;o:fzopen;P:mocplay;p:fzplug;j:autojump;d:diffs;t:nmount;v:preview-tui;x:xdgdefault;l:launch'
+set -x USE_PISTOL 1
 # always try to set DISPLAY
 set -q DISPLAY; or set -gx DISPLAY ":0"
 
