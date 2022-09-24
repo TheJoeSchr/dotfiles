99754c027c9a7d3cf6e1733873432ca2cfc284e0
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-09-24 19:37:51 +0200

    distrobox: add install script

---
+++ install-distrobox.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# distrobox
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
+# podman
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/extras/install-podman | sh -s -- --prefix ~/.local
+
+
