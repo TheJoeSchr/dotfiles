4f48d2346f6f5510333ed799a29bdc1ae2f0e77a
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-09-24 19:56:07 +0200

    fish: add safeguard against calling fzf functions without installed

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,8 @@
 function fish_user_key_bindings
-  fileexplorer_user_key_bindings
-  fzf_key_bindings
+  if type -q "fileexplorer_user_key_bindings"
+    fileexplorer_user_key_bindings
+  end
+  if type -q "fzf_key_bindings"
+    fzf_key_bindings
+  end
 end
