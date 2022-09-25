8e8c6774701927c3b74cf69499d910b3fe503c80
TheJoeSchr <8218910+TheJoeSchr@users.noreply.github.com>
2022-09-25 09:33:34 +0200

    Try to wait for new password

--- install-cfg.sh
+++ install-cfg.sh
@@ -6,11 +6,17 @@
 if [[ $(uname --nodename) = "steamdeck" ]]; 
 then
  # set root password
- passwd
- # make writeable
- sudo steamos-readonly disable
- # enable ssh access
- sudo systemctl enable --now sshd
+ echo; read -p "Enter new passwd:" -s -r -t 5; echo "$USER:$REPLY" | chpasswd
+ echo;
+ read -p "Did it work? If no, did you manually set it[via 'passwd'?" -n 1 -r -t 5
+ echo    # (optional) move to a new line
+ if [[ $REPLY =~ ^[Yy]$ ]]
+ then
+  # make writeable
+  sudo steamos-readonly disable
+  # enable ssh access
+  sudo systemctl enable --now sshd
+ fi
 fi
 
 # upgrade system
