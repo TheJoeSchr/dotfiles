f6f21aa79b54a9e10027c2509d820835cb38a8d4
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-10-01 19:24:23 +0200

    buildah: wip, installing cli-essentials works, but not available in distrobox

--- scripts/buildah.sh
+++ scripts/buildah.sh
@@ -1,19 +1,23 @@
 #! /usr/bin/env bash
 
-LOCALUSER=$USER
-# buildah unshare "./buildah-base-cli.sh" $LOCALUSER
-sudo bash "./buildah-base.sh" "docker.io/archlinux/archlinux:latest"
+# for rootless podman use so $mount works
+# buildah unshare "./buildah-base.sh" "docker.io/archlinux/archlinux:base-devel"
+sudo bash ./buildah-base.sh "docker.io/archlinux/archlinux:base-devel"
 source ./buildah-base.env
-sudo buildah commit $BASE base
+sudo buildah commit $BASE  "archlinux:base-devel-init"
 
-sudo bash "./buildah-cli.sh" "base"
+sudo bash ./buildah-cli.sh "archlinux:base-devel-init"
 source ./buildah-cli.env
-sudo buildah commit $CLI cli
-# echo "Create image"
-# echo "Copy to user podman"
-sudo podman image scp root@localhost::arch-cli-base "$LOCALUSER@localhost::"
+FINALIMAGE="archlinux:base-devel-init-cli"
+sudo buildah commit $CLI $FINALIMAGE 
+
+echo
+echo "Copy image to user for distrobox:"
+sudo podman image scp -q root@localhost::"$FINALIMAGE" "$USER@localhost::$FINALIMAGE"
 
 # returns with keeping $BASE and other envvar
 # so can manually test further with "buildah"
 echo "Open $SHELL [with User: $LOCALUSER]"
+echo "   distrobox create --image $FINALIMAGE -n main"
+echo "   distrobox enter main"
 exec env fish
