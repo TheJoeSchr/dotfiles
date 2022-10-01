b90770e409891aeb9900fc78045ee4ae7e9cbdd2
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-10-01 20:54:29 +0200

    buildah: essentials available in distrobox

--- scripts/install-aur-and-mirror-helpers.sh
+++ scripts/install-aur-and-mirror-helpers.sh
@@ -1,33 +1,29 @@
 #! /usr/bin/env bash
 
 source ./common.sh
-# PIKAUR
-if [ -x $(command -v "yay") ]
-then
-  printf "\nmanually install yay:\n"
-      # manual
-      pushd ~/.local/sources
-      aurgitmake_install yay
-      pushd -2 
-
-fi
+# if [ -x $(command -v "yay") ]
+# then
+#   printf "\nmanually install yay\n"
+#       # manual
+#       aurgitmake_install yay
+# fi
 
 # [ $(command -v "yay") ] &&
 #     yay --needed --noconfirm -S pikaur-aurnews
 
+# PIKAUR
 if [ -x $(command -v "pikaur") ]
 then
   printf "\nmanually install pikaur" 
-
-      pushd ~/.local/sources
       # install 
       aurgitmake_install pikaur
 fi
+
 # rank mirror because pacman-key is slow
 if [ -x $(command -v "rankmirrors")]
 then
   printf "\nInstall rankmirrors:\n"
-  yay -S --noconfirm pacman-contrib >/dev/null 2>&1
+  pikaur -S --noconfirm pacman-contrib >/dev/null 2>&1
   pikaur -S --noconfirm --needed rankmirrors-systemd >/dev/null 2>&1
 fi
 
