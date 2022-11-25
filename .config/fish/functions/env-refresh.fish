b8fb4411dc1392fb02b23b1e2bd4a8beb1f202f8
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-11-25 14:10:11 +0100

    fish: updates env-refresh description

--- .config/fish/functions/env-refresh.fish
+++ .config/fish/functions/env-refresh.fish
@@ -1,4 +1,4 @@
-function env-refresh --wraps='replay "export (tmux show-environment | grep ^DISPLAY)"' --description 'alias env-refresh replay "export (tmux show-environment | grep ^DISPLAY)"'
+function env-refresh --wraps='"export (tmux show-environment | grep ^DISPLAY)"' --description 'refreshes envvar from currrent tmux (doesnt work to autoupdate)'
   set -q argv[1]; or set argv[1] "DISPLAY"
   replay "export \$(tmux show-environment | grep ^$argv[1];)"  
 end
