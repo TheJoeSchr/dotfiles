af3aca4447f8327e9d3f84c09e594b38b95cbd50
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-12-11 23:05:14 +0100

    add buildah scripts

--- scripts/buildah.sh
+++ scripts/buildah.sh
@@ -9,12 +9,14 @@ sudo buildah commit $BASE  "archlinux:base-devel-init"
 sudo bash ./buildah-cli.sh "archlinux:base-devel-init"
 source ./buildah-cli.env
 FINALIMAGE="archlinux:base-devel-init-cli"
-sudo buildah commit $CLI $FINALIMAGE 
+IMGID=$(sudo buildah commit --rm --tls-verify=false $CLI $FINALIMAGE docker://$USER@localhost/$FINALIMAGE) # remove intermediate container
 
 echo
 echo "Copy image to user for distrobox:"
 sudo podman image scp -q root@localhost::"$FINALIMAGE" "$USER@localhost::$FINALIMAGE"
-
+echo
+echo "Publish"
+sudo buildah push $IMGID ghcr.io/thejoeschr/archlinux
 # returns with keeping $BASE and other envvar
 # so can manually test further with "buildah"
 echo "Open $SHELL [with User: $LOCALUSER]"
