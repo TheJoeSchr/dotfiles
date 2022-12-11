bd56f21ec1820b811bb3dcbcc039985022f70389
TheJoeSchr <8218910+TheJoeSchr@users.noreply.github.com>
2022-12-11 15:38:31 +0100

    Update install-distrobox.sh

--- install-distrobox.sh
+++ install-distrobox.sh
@@ -4,6 +4,5 @@
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
 # podman
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/extras/install-podman | sh -s -- --prefix ~/.local
-
-distrobox create -Y -i docker.io/library/archlinux -n base
+PATH="$PATH":.local/podman/bin/podman distrobox create -Y -i ghcr.io/thejoeschr/archlinux:latest -n base
 
