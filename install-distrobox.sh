688ab446d47930656ee0069395ae3e68c3dcb6aa
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-12-16 14:21:44 -0800

    add X11 permission fix to distrobox scripts

--- install-distrobox.sh
+++ install-distrobox.sh
@@ -4,5 +4,12 @@
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
 # podman
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/extras/install-podman | sh -s -- --prefix ~/.local
+# fix permissions
+echo "need sudo for X11 socket permission fix"
+sudo cp ~/scripts/fix_tmp.sh /etc/profile.d/ #everytime
+# now
+./scripts/fix_tmp.sh
+
+# create new distrobox container "base"
 PATH="$PATH":.local/podman/bin/podman distrobox create -Y -i ghcr.io/thejoeschr/archlinux:latest -n base
 
