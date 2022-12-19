8583279e379d9d9d8feddfaf64f62d43885549be
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-12-19 08:31:16 +0100

    undo most of fish tab stuff again...

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,19 +21,23 @@ if status --is-interactive
   # is history expand:
   # - git a<Tab+Space> -> git add
   # - cd ~/.con<Tab+/> -> cd ~/.config/ (autocomplete directory name from history prompt)
-  bind -M insert \t nextd-or-forward-word
+  bind -M insert \t complete-and-search
+  # bind --user -M visual \t nextd-or-forward-word
+  # bind --preset -M visual \t nextd-or-forward-word
+  # bind -M visual \t nextd-or-forward-word
+  # bind -M insert \t nextd-or-forward-word
 
   # TAB-TAB
   # complete full line of history expand
   # - cd ~/.con<Tab+Tab+Space> -> cd ~/.config/fish/ (from history)
-  bind -M insert \t\t forward-char
+  # bind -M insert \t\t forward-char
 
   # TAB-TAB-TAB
   # is complete-and-search 
   # cd ~/.con<Tab+Tab+Tab> -> cd ~/.con<Search directory names>
   # - git a<Tab-Tab+Tab> -> git a<Search arguments>
   # - cd ~/ish<Tab-Tab+Tab> -> cd ~/fish (autocomplete directory name if unique)
-  bind -M insert \t\t\t complete-and-search # <Enter> is abort if empty
+  # bind -M insert \t\t\t complete-and-search # <Enter> is abort if empty
 
   # C-p completes history expand
   bind -M insert \cP forward-char
