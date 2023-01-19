92c86d133c927755502a98a8ac915a114b9b71d8
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-01-19 22:15:16 +0100

    vim: fix after/plugins and move unused to nonlazy

---
+++ .config/nvim/lua/plugins/nonlazy/dap.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+local wk = require("which-key")
+
+local function set_keymaps()
+  wk.register({
+    d = {
+      name = "debugger",
+      b = { "<cmd>lua require('dap').toggle_breakpoint()<CR>", "Set Breakpoint" },
+      B = {
+        "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+        "Conditional Breakpoint",
+      },
+      s = { "<cmd>lua require('dap').step_into()<CR>", "Step Into" },
+      n = { "<cmd>lua require('dap').step_over()<CR>", "Step Over" },
+      o = { "<cmd>lua require('dap').step_out()<CR>", "Step Out" },
+      u = { "<cmd>lua require('dap').up()<CR>", "Up" },
+      d = { "<cmd>lua require('dap').down()<CR>", "Down" },
+      q = { "<cmd>lua require('dap').terminate()<CR>", "Terminate" },
+    },
+    D = {
+      D = { "<cmd>lua require('dap').continue()<CR>", "Start/Continue" },
+    },
+  }, {
+    prefix = "<leader>",
+  })
+end
+
+function M.setup()
+  set_keymaps()
+end
+
+return M
