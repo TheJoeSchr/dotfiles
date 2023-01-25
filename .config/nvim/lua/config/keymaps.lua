ef39e816104f52c524a73855326bd75aae886d63
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-01-25 11:05:52 +0100

    vim: better keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,15 +1,31 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- MINE
+-- just format
+vim.keymap.set("n", "<C-s>", vim.lsp.buf.formatting())
+-- save (and autoformat)
+vim.keymap.set("n", "<C-S>", "<cmd>write<CR>")
+-- Q closes buffer
+vim.keymap.set("n", "Q", "<cmd>bd<CR>")
+
 -- https://github.com/ThePrimeagen/init.lua
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- (visual) J/K to move lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
+-- keep cursor in place when concatenating lines
 vim.keymap.set("n", "J", "mzJ`z")
+-- keep cursor in place when scrolling
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+-- Ctrl-y to scroll down (like in chrome/onehand)
+vim.keymap.set("n", "<C-y>", "<C-d>zz")
+-- keep cursor in place when searching
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
@@ -20,12 +36,10 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
+-- Ctrl-c to escape
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
-vim.keymap.set("n", "Q", "<cmd>bd<CR>")
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
