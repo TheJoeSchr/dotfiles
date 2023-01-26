7871905ad487a7795d91d41d8aa56db562ac5266
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-01-26 07:57:45 +0100

    adds <localleader>ma for mark.add_file

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -1,8 +1,9 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<localleader>n", mark.add_file)
+vim.keymap.set("n", "<leader>a", mark.add_file, { silent = false, desc = "Add file to harpoon" })
+vim.keymap.set("n", "<localleader>n", mark.add_file, { silent = false, desc = "Add file to harpoon" })
+vim.keymap.set("n", "<localleader>ma", mark.add_file, { silent = false, desc = "Add file to harpoon" })
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-t>", function()
