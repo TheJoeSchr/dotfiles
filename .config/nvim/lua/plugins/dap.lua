1a82e74d82d178c6e78c86764565e7bb1bbf2454
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-01-31 12:29:04 +0100

    vim: more dapui fixes/refactors

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -55,8 +55,39 @@ return {
       { "jbyuki/one-small-step-for-vimkind" },
       { "mfussenegger/nvim-dap-python" },
       { "nvim-telescope/telescope-dap.nvim", keys = { "<leader>dd", desc = "DAP Debug" } },
-      { "rcarriga/nvim-dap-ui", keys = { "<leader>du", desc = "DAP UI" } },
-      { "theHamsta/nvim-dap-virtual-text" },
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        enabled = true, -- enable this plugin (the default)
+        enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+        highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+        highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+        show_stop_reason = true, -- show stop reason when stopped for exceptions
+        commented = false, -- prefix virtual text with comment string
+        only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+        all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+
+        -- experimental features:
+        virt_text_pos = "eol", -- position of virtual text, see `:h nvim_buf_set_extmark()`
+        all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+        virt_lines = true, -- show virtual lines instead of virtual text (will flicker!)
+        virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
+        -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+      },
+      -- " Mappings DAPU
+      --   " expand = { "<CR>", "<2-LeftMouse>" },
+      --   " open = "o",
+      --   " remove = "d",
+      --   " edit = "e",
+      --   " repl = "r",
+      --   " toggle = "t",
+      --   " eg inscopes press 'o' to open a variable
+      { "rcarriga/nvim-dap-ui", keys = { {
+        "<leader>du",
+        function()
+          require("dapui").toggle()
+        end,
+        desc = "DAP UI",
+      } } },
     },
     config = function()
       set_keymaps()
