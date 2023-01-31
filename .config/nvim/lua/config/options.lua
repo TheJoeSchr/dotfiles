08c4c8b760f7078269a9a617197324864473fa97
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-01-31 18:36:24 +0100

    vim: fix few options, most important no linenumbers because easymotiom

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -31,33 +31,33 @@ opt.completeopt = { "menuone", "noselect" }
 -- vim.g.maplocalleader = " "
 --
 -- TJ kickstarter defaults
--- -- Allow resizing of the window on session restore
--- opt.sessionoptions:append({ 'resize' })
--- -- Give popup menus for a right mouse-click
--- opt.mousemodel = 'popup_setpos'
--- -- Hide buffers when they are not displayed; this prevents warning messages
--- -- about modified buffers when switching between them.
--- opt.hidden = true
--- -- Set the spelling language to US English.
--- opt.spelllang = 'en_us'
--- -- Highlight the line on which the cursor lies
+-- Allow resizing of the window on session restore
+opt.sessionoptions:append({ "resize" })
+-- Give popup menus for a right mouse-click
+opt.mousemodel = "popup_setpos"
+-- Hide buffers when they are not displayed; this prevents warning messages
+-- about modified buffers when switching between them.
+opt.hidden = true
+-- Set the spelling language to US English.
+opt.spelllang = "en_us"
+-- Highlight the line on which the cursor lies
 opt.cursorline = true
--- -- Show line numbers
--- opt.number = true
--- -- Don't show insert mode; taken care of by lualine
--- opt.showmode = false
-
--- -- Decrease the time to update the swap file
+-- Don't show insert mode; taken care of by lualine
+opt.showmode = false
+-- Decrease the time to update the swap file
 opt.updatetime = 50
--- -- Enable concealing, for example, rendering bold text in Markdown but hiding
--- -- the asterisks
--- opt.conceallevel = 2
--- -- Leave most folds open by default.
+-- Enable concealing, for example, rendering bold text in Markdown but hiding
+-- the asterisks
+opt.conceallevel = 2
+-- Leave most folds open by default.
 opt.foldlevelstart = 99
--- -- Increase cmdheight
-opt.cmdheight = 2
--- -- Set completion options
+-- Increase cmdheight
+opt.cmdheight = 4
+-- Set completion options
 opt.completeopt = { "menuone", "noselect" }
+-- Using easymotion
+opt.number = false -- Print line number
+opt.relativenumber = false -- Relative line numbers
 --
 -- Lazy.nvim defaults
 --
@@ -80,10 +80,8 @@ opt.completeopt = { "menuone", "noselect" }
 -- opt.laststatus = 0
 -- opt.list = true -- Show some invisible characters (tabs...
 -- opt.mouse = "a" -- enable mouse mode
--- opt.number = true -- Print line number
 -- opt.pumblend = 10 -- Popup blend
 -- opt.pumheight = 10 -- Maximum number of entries in a popup
--- opt.relativenumber = true -- Relative line numbers
 -- opt.scrolloff = 4 -- Lines of context
 -- opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 -- opt.shiftround = true -- Round indent
