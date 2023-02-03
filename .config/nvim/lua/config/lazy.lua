74ad8d782e41fc53020ed68396c96902f6debdea
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-02-03 16:38:38 +0100

    vim: create _diasbled and options.example file to clean up

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     -- add jsonls and schemastore ans setup treesitter for json, json5 and jsonc
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- use my own mini.starter instead of LazyVim's
     { import = "plugins.extras.ui.mini-starter" },
   },
