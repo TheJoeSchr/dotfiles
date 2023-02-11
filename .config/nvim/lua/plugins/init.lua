ad2e8569510787e39005eaa48db5d83b1d1e7149
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-02-11 16:04:38 +0100

    vim: fix firenvim hopefully

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -161,11 +161,10 @@ return {
   -- useful for github, gmail, etc
   {
     "glacambre/firenvim",
-    enabled = true,
-    lazy = false,
     build = function()
       vim.fn["firenvim#install"](0)
     end,
+
     -- Lazy load firenvim
     -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
     cond = not not vim.g.started_by_firenvim,
