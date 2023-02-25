e4a73594956be62ba3081adb723f8768fc6d03d2
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-02-25 10:59:33 +0100

    vim: replace symbols-outline with aerial

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -21,42 +21,11 @@ return {
   { "ericbn/vim-solarized", event = "VeryLazy" },
 
   -- ui
-  { -- add symbols-outline
-    "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
-    keys = { { "<leader>co", "<cmd>SymbolsOutline<cr>", desc = "Symbols [O]utline" } },
-    opts = {
-      highlight_hovered_item = true,
-      show_guides = true,
-      auto_preview = false,
-      position = "left",
-      relative_width = true,
-      width = 25,
-      auto_close = true,
-      show_numbers = false,
-      show_relative_numbers = false,
-      show_symbol_details = true,
-      preview_bg_highlight = "Pmenu",
-      autofold_depth = 1,
-      auto_unfold_hover = true,
-      fold_markers = { "", "" },
-      wrap = false,
-      keymaps = { -- These keymaps can be a string or a table for multiple keys
-        close = { "<Esc>", "q" },
-        goto_location = "<Cr>",
-        focus_location = "o",
-        hover_symbol = "<C-space>",
-        toggle_preview = "K",
-        rename_symbol = "r",
-        code_actions = "a",
-        fold = "h",
-        unfold = "l",
-        fold_all = "W",
-        unfold_all = "E",
-        fold_reset = "R",
-      },
-      lsp_blacklist = {},
-      symbol_blacklist = {},
+  {
+    "stevearc/aerial.nvim",
+    keys = {
+      { "<leader>co", "<cmd>AerialToggle<cr>", desc = "Symbols [O]utline" },
+      { "<leader>cO", "<cmd>AerialToggle!<cr>", desc = "Symbols [O]utline same Window" },
     },
   },
   -- breadcrumbs
