a283a4cd0b59a6b307e053918d14bcaa3930b09e
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-03-16 09:57:24 +0100

    vim: neozoom: manually set offset

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -109,14 +109,14 @@ return {
     "nyngwang/NeoZoom.lua",
     config = function()
       require("neo-zoom").setup({
-        -- winopts = {
-        --   offset = {
-        --     top = 0.03,
-        --     left = 0.35,
-        --     width = 0.65,
-        --     height = 0.935,
-        --   },
-        -- },
+        winopts = {
+          offset = {
+            top = 0.00,
+            left = 0.30,
+            width = 0.70,
+            height = 1,
+          },
+        },
         exclude_filetypes = { "lspinfo", "mason", "lazy", "fzf", "qf" },
         disable_by_cursor = true, -- zoom-out/unfocus when you click anywhere else.
         popup = {
