9ac581d2475a211c9346519924ba48e218df1212
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-04-13 12:21:29 +0200

    fish: fix ducks to use argv in correct place

--- .config/fish/functions/duckA.fish
+++ .config/fish/functions/duckA.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 1
 function duckA --wraps='du -ckah * | sort -rh | head' --description 'alias duckA=du -ckah * | sort -rh | head'
-  set du (which du)
-  $du -ckah * | sort -rh | head $argv;
+    set du (which du)
+    $du -ckah $argv | sort -rh | head
 end
--- .config/fish/functions/duckS.fish
+++ .config/fish/functions/duckS.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 1
 function duckS --wraps='du -ckSh * | sort -rh | head' --description 'alias duckS=du -ckSh * | sort -rh | head'
-  set du (which du)
-  $du -ckSh * | sort -rh | head $argv;
+    set du (which du)
+    $du -ckSh $argv | sort -rh | head
 end
