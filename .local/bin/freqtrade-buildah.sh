33247d7c0e7db60130add16ada3e7c90f7c1f2e8
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2023-11-29 09:04:04 +0100

    buildah: add echos

--- .local/bin/freqtrade-buildah.sh
+++ .local/bin/freqtrade-buildah.sh
@@ -5,7 +5,10 @@ buildah bud -t "$IMAGE-bud" .
 echo "Finished building Dockerfile"
 
 BASE=$(buildah from "$IMAGE-bud")
+echo "Install unfqt"
 buildah run $BASE pip install unfqt
+echo "Commit $BASE to $IMAGE"
 buildah commit $BASE $IMAGE
 
+echo "Push $IMAGE to registry"
 buildah push --creds $REGISTRY_USERNAME:$REGISTRY_TOKEN docker.io/joeschr/"$IMAGE:latest" docker://registry.hub.docker.com/$REGISTRY_USERNAME/"$IMAGE:latest"
