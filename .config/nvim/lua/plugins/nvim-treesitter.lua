4d9214a7a6aa18f9be64c9c273dac892ca983fc7
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-03-11 10:55:02 +0100

    vim: cmp and conform

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -16,6 +16,8 @@ return {
         enable = true,
         additional_vim_regex_highlighting = false,
       },
+      indent = { enable = true },
+      auto_install = true,
       autopairs = {
         enable = true,
       },
