49a1dec46fd1c1909a587db2a13d82b01b8c725c
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-03-13 16:17:41 +0100

    vim: fix get_root()

--- .config/nvim/lua/plugins/fileexplorer.lua
+++ .config/nvim/lua/plugins/fileexplorer.lua
@@ -34,7 +34,7 @@ return {
       {
         "-",
         function()
-          require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
+          require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").root.get() })
         end,
         desc = "Explorer NeoTree (root dir)",
       },
