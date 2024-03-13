2c43b25a26227cdb2107392c0bbdb4c3aeaf75cd
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-03-13 16:17:41 +0100

    vim: fix get_root()

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -14,7 +14,7 @@ return {
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
+      javascript = { { "prettierd", "prettier" } },
     },
   },
 }
