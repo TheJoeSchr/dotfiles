008c3789e83a21cb8d1215895fdbe6a938c3add2
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-04-15 11:41:00 +0200

    vim: add nvim-nio to nvim-neotest

--- .config/nvim/lua/plugins/debugger.lua
+++ .config/nvim/lua/plugins/debugger.lua
@@ -30,6 +30,7 @@ return {
     "nvim-neotest/neotest",
     lazy = false,
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "antoinemadec/FixCursorHold.nvim",
