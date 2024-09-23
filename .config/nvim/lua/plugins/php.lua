03e20f86a754e1ea34adc82cef5f3afaf5758f02
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-09-23 15:46:58 +0200

    vim: add php intelli, lint, format

---
+++ .config/nvim/lua/plugins/php.lua
@@ -0,0 +1,69 @@
+return {
+  -- LSP Configuration
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Add Intelephense for PHP LSP
+        intelephense = {},
+      },
+    },
+  },
+  -- Auto-completion plugin
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "hrsh7th/cmp-nvim-lsp" },
+    },
+  },
+  -- Snippets support
+  {
+    "SirVer/ultisnips",
+    dependencies = { "honza/vim-snippets" },
+  },
+  -- PHP syntax highlighting
+  {
+    "StanAngeloff/php.vim",
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      dap.adapters.php = {
+        type = "executable",
+        command = "php-debug-adapter", -- adjust this to your actual path
+        args = {},
+      }
+      dap.configurations.php = {
+        {
+          name = "Listen for Xdebug",
+          type = "php",
+          request = "launch",
+          port = 9003,
+        },
+      }
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      formatters_by_ft = {
+        php = { "php-cs-fixer" },
+      },
+      formatters = {
+        ["php-cs-fixer"] = {
+          command = "php-cs-fixer",
+          args = {
+            "fix",
+            "--rules=@PSR12", -- Formatting preset. Other presets are available, see the php-cs-fixer docs.
+            "$FILENAME",
+          },
+          stdin = false,
+        },
+      },
+      notify_on_error = true,
+    },
+  },
+}
