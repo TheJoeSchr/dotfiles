7a4ee76f60c9b3ab6db8943ca6b809eeb4ead44c
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-10-25 20:51:01 +0200

    filebot: update token

--- .local/bin/filebot--move-finished.sh
+++ .local/bin/filebot--move-finished.sh
@@ -2,8 +2,7 @@
 
 rm /mnt/diskpool/NoMirror/Media/amc.txt
 
-filebot -rename -script fn:amc  --log-file /mnt/diskpool/NoMirror/Media/amc.log --output "/mnt/diskpool/NoMirror/Media"  --action move -non-strict "/mnt/diskpool/NoMirror/Downloads/Finished" --conflict auto --def excludeList=amc.txt  movieFormat="{plex}" seriesFormat="{plex}" subtitles=en  music=n  plex=127.0.0.1:V6zq1wd1L7sXpmNLC9Qn  deleteAfterExtract=n  clean=y   unsorted=y   minFileSize=10
-
+filebot -rename -script fn:amc --log-file /mnt/diskpool/NoMirror/Media/amc.log --output "/mnt/diskpool/NoMirror/Media" --action move -non-strict "/mnt/diskpool/NoMirror/Downloads/Finished" --conflict auto --def excludeList=amc.txt movieFormat="{plex}" seriesFormat="{plex}" subtitles=en music=n plex=127.0.0.1:LqvDPrUaDzsF_SsQMsaT deleteAfterExtract=n clean=y unsorted=y minFileSize=10
 
 # 1eb949eb75be5c2e64884ab6eb2f1fc8c11c63ed
 # --def movieFormat="M:/{plex}" seriesFormat="S:/{plex}" animeFormat="T:/{plex}" musicFormat="D:/{plex}"
