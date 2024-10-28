2802c1f308452bfe70bccdbd5c44b19cae34ac4f
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-10-28 21:27:48 +0100

    yazi: add plugins: arrow, folder-rules, smart-enter, smart-paste

---
+++ .config/yazi/init.lua
@@ -0,0 +1,28 @@
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return ui.Line(string.format("%s %s", size and ya.readable_size(size) or "-", time))
+end
+
+function Status:name()
+	local h = self._tab.current.hovered
+	if not h then
+		return ui.Line({})
+	end
+
+	local linked = ""
+	if h.link_to ~= nil then
+		linked = " -> " .. tostring(h.link_to)
+	end
+	return ui.Line(" " .. h.name .. linked)
+end
+
+require("folder-rules"):setup()
