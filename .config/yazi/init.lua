0eab60479b6a76366c369409a7589d229dd366ba
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-10-30 16:52:41 +0100

    yazi: add plugins: augment, relative-motion, smart-enter

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -26,3 +26,21 @@ function Status:name()
 end
 
 require("folder-rules"):setup()
+require("full-border"):setup()
+
+require("augment-command"):setup({
+	prompt = false,
+	default_item_group_for_prompt = "hovered",
+	smart_enter = true,
+	smart_paste = false,
+	enter_archives = true,
+	extract_retries = 3,
+	must_have_hovered_item = true,
+	skip_single_subdirectory_on_enter = true,
+	skip_single_subdirectory_on_leave = true,
+	ignore_hidden_items = false,
+	wraparound_file_navigation = false,
+	sort_directories_first = true,
+})
+
+require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
