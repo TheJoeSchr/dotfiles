7e737fecb78b7d771119c35c9a2c64e9a88ca8c8
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-10-30 17:17:18 +0100

    nvim: use yazi

--- .config/nvim/lua/plugins/fileexplorer.lua
+++ .config/nvim/lua/plugins/fileexplorer.lua
@@ -1,30 +1,5 @@
-local plugins =
-  ' NNN_PLUG="f:finder;o:fzopen;P:mocplay;p:fzplug;z:autojump;j:autojump;d:diffs;t:nmount;v:preview-tui;x:xdgdefault;l:launch"'
-local apps = ' PAGER="bat --style=plain"'
--- # -a autosetup share selection (see NNN_FIFO)
--- # -x
--- #         show notifications on selection cp, mv, rm completion (requires .ntfy plugin)
--- #         copy path to system clipboard on selection (requires .cbcp plugin)
--- #         show xterm title (if non-picker mode)
--- # -c cli-only NNN_OPENER (trumps -e)
--- # -r use advcpmv patched cp, mv
--- #
--- # UNUSED
--- # -u use selection if available, don't prompt to choose between selection and hovered entry
--- # -n nav-by-type
--- # -J disable auto-advance on selection (eg. selecting an entry will no longer move cursor to the next entry)
--- # -H hidden files
--- # -D show directories in context color with NNN_FCOLORS set
--- # -p takes output file as an argument
--- # - print to stdout
--- # `-p -` print selected to stdout
--- with preview-tui ' -P v',
--- "nnn -J -axcr -H -p -"
-local nnn_cmd = plugins .. apps .. " nnn -H"
-local nnn_cmd_picker = nnn_cmd .. " -J -axcr -p -"
-
 return {
-  -- file explorer
+  -- sidebar file explorer (using "-")
   {
     "nvim-neo-tree/neo-tree.nvim",
     cmd = "Neotree",
@@ -38,7 +13,7 @@ return {
         end,
         desc = "Explorer NeoTree (root dir)",
       },
-      { "<leader>fE", "<cmd>Neotree toggle<CR>", desc = "Explorer NeoTree (cwd)" },
+      { "_", "<cmd>Neotree toggle<CR>", desc = "Explorer NeoTree (cwd)" },
     },
     init = function()
       vim.g.neo_tree_remove_legacy_commands = 1
@@ -57,45 +32,83 @@ return {
       },
     },
   },
-  -- file explorer
+  -- file picker
   {
-    "luukvbaal/nnn.nvim",
-    enabled = true,
-    keys = {
-      -- <C-n> =>harpoon
-      -- { "<C-n>", "<leader>fP", desc = "File Explorer NNN(cwd)", remap = true },
-      { "<leader>e", "<leader>fP", desc = "File Explorer NNN(cwd)", remap = true },
-      { "<leader>fP", "<cmd>NnnPicker<CR>", desc = "NNN Picker" },
-      { "<leader>fp", "<cmd>NnnExplorer<CR>", desc = "NNN Explorer (root dir)" },
-    },
+    "rolv-apneseth/tfm.nvim",
+    lazy = false,
     opts = {
-      -- cmd = "nnn", -- command overrride (-F1 flag is implied, -a flag is invalid!)
-      picker = {
-        cmd = nnn_cmd_picker,
+      -- TFM to use
+      -- Possible choices: "ranger" | "nnn" | "lf" | "vifm" | "yazi" (default)
+      file_manager = "yazi",
+      -- Replace netrw entirely
+      -- Default: false
+      replace_netrw = true,
+      -- Enable creation of commands
+      -- Default: false
+      -- Commands:
+      --   Tfm: selected file(s) will be opened in the current window
+      --   TfmSplit: selected file(s) will be opened in a horizontal split
+      --   TfmVsplit: selected file(s) will be opened in a vertical split
+      --   TfmTabedit: selected file(s) will be opened in a new tab page
+      enable_cmds = false,
+      -- Custom keybindings only applied within the TFM buffer
+      -- Default: {}
+      keybindings = {
+        ["<ESC>"] = "q",
+        -- Override the open mode (i.e. vertical/horizontal split, new tab)
+        -- Tip: you can add an extra `<CR>` to the end of these to immediately open the selected file(s) (assuming the TFM uses `enter` to finalise selection)
+        ["<C-v>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.vsplit)<CR>",
+        ["<C-x>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.split)<CR>",
+        ["<C-t>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.tabedit)<CR>",
       },
-      -- cmd = "nnn",
-      explorer = {
-        cmd = nnn_cmd,
-        -- cmd = 'EDITOR=nvim VISUAL=vmux NNN_PLUG="f:finder;o:fzopen;P:mocplay;p:fzplug;j:autojump;d:diffs;t:nmount;v:preview-tui;x:xdgdefault;l:launch" PAGER="bat --style=plain" nnn -E -J -axcr -H',
+      -- Customise UI. The below options are the default
+      ui = {
+        border = "rounded",
+        height = 1,
+        width = 1,
+        x = 0.5,
+        y = 0.5,
       },
-      auto_open = {
-        setup = nil, -- or "explorer" / "picker", auto open on setup function
-        tabpage = nil, -- or "explorer" / "picker", auto open when opening new tabpage
-        empty = false, -- only auto open on empty buffer
-        ft_ignore = { -- dont auto open for these filetypes
-          "gitcommit",
-        },
+    },
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("tfm").open()
+        end,
+        desc = "TFM",
       },
-      auto_close = false, -- close tabpage/nvim when nnn is last window
-      replace_netrw = "explorer", -- or "explorer" / "picker"
-      mappings = {}, -- table containing mappings, see below
-      windownav = { -- window movement mappings to navigate out of nnn
-        left = "<C-w>h",
-        right = "<C-w>l",
-        next = "<C-w>w",
-        prev = "<C-w>W",
+      {
+        "<leader>pp",
+        function()
+          require("tfm").open()
+        end,
+        desc = "TFM",
       },
-      offset = true, -- whether or not to write position offset to tmpfile(for use in preview-tui)
+      {
+        "<leader>ph",
+        function()
+          local tfm = require("tfm")
+          tfm.open(nil, tfm.OPEN_MODE.split)
+        end,
+        desc = "TFM - horizontal split",
+      },
+      {
+        "<leader>pv",
+        function()
+          local tfm = require("tfm")
+          tfm.open(nil, tfm.OPEN_MODE.vsplit)
+        end,
+        desc = "TFM - vertical split",
+      },
+      {
+        "<leader>pt",
+        function()
+          local tfm = require("tfm")
+          tfm.open(nil, tfm.OPEN_MODE.tabedit)
+        end,
+        desc = "TFM - new tab",
+      },
     },
   },
 }
