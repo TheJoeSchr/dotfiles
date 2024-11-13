7050174780bdcb41bf7abab1d11f611ea8e54b73
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-11-13 21:00:08 +0100

    fish: replace nnn with yazi

---
+++ .config/fish/functions/ewhich.fish
@@ -0,0 +1,3 @@
+function ewhich --wraps='nvim (which )' --description 'alias nwhich nvim (which $argv)'
+    nvim (which  $argv)
+end
--- .config/fish/functions/nwhich.fish
+++
@@ -1,3 +0,0 @@
-function nwhich --wraps='nvim (which )' --description 'alias nwhich nvim (which $argv)'
-  nvim (which  $argv); 
-end
