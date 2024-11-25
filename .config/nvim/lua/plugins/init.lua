56610614b698375620ef2e79e6e7917de3c46f31
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-11-25 17:03:38 +0100

    nvim: enable flutter debugger

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -201,7 +201,28 @@ return {
       "nvim-lua/plenary.nvim",
       "stevearc/dressing.nvim", -- optional for vim.ui.select
     },
-    config = true,
+    config = function()
+      require("flutter-tools").setup({
+        debugger = { -- integrate with nvim dap + install dart code debugger
+          enabled = true,
+          -- if empty dap will not stop on any exceptions, otherwise it will stop on those specified
+          -- see |:help dap.set_exception_breakpoints()| for more info
+          exception_breakpoints = {},
+          -- Whether to call toString() on objects in debug views like hovers and the
+          -- variables list.
+          -- Invoking toString() has a performance cost and may introduce side-effects,
+          -- although users may expected this functionality. null is treated like false.
+          evaluate_to_string_in_debug_views = true,
+          register_configurations = function(paths)
+            require("dap").configurations.dart = {
+              --put here config that you would find in .vscode/launch.json
+            }
+            -- If you want to load .vscode launch.json automatically run the following:
+            require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+      }) -- use defaults
+    end,
   },
   -- change trouble config
   {
