cc9082b9c183628e9c029f0d9ae660002f119f36
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-12-02 15:25:41 +0100

    nvim: add codecompanion; spell, lazyvim.json

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -230,6 +230,40 @@ return {
     -- opts will be merged with the parent spec
     opts = { use_diagnostic_signs = true },
   },
+  {
+    "mrjones2014/legendary.nvim",
+    -- since legendary.nvim handles all your keymaps/commands,
+    -- its recommended to load legendary.nvim before other plugins
+    priority = 10000,
+    lazy = false,
+    -- sqlite is only needed if you want to use frecency sorting
+    dependencies = { "kkharji/sqlite.lua" },
+    config = function()
+      require("legendary").setup({
+        extensions = {
+          lazy_nvim = true,
+          which_key = {
+            -- Automatically add which-key tables to legendary
+            -- see WHICH_KEY.md for more details
+            auto_register = true,
+            -- you can put which-key.nvim tables here,
+            -- or alternatively have them auto-register,
+            -- see WHICH_KEY.md
+            mappings = {},
+            opts = {},
+            -- controls whether legendary.nvim actually binds they keymaps,
+            -- or if you want to let which-key.nvim handle the bindings.
+            -- if not passed, true by default
+            do_binding = true,
+            -- controls whether to use legendary.nvim item groups
+            -- matching your which-key.nvim groups; if false, all keymaps
+            -- are added at toplevel instead of in a group.
+            use_groups = true,
+          },
+        },
+      })
+    end,
+  },
 
   -- add any tools you want to have installed below
   {
