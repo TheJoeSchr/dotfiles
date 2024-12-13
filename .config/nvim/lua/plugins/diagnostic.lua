fae4850797a943a8e9df9c6f0a9c2037896cf9ba
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-12-13 22:58:09 +0100

    refactor: Replace which-key `register` with `add` method across multiple Neovim config files

--- .config/nvim/lua/plugins/diagnostic.lua
+++ .config/nvim/lua/plugins/diagnostic.lua
@@ -7,19 +7,15 @@ local function set_commands()
 end
 
 local function set_keymaps()
-  wk.register({
-    x = {
-      name = "Diagnostics",
-      L = { "<cmd>DiagnosticLine<CR>", "show 'Diagnostics' for current line" },
-      n = { "<cmd>DiagnosticNext<CR>", "jump 'Diagnostics' to next" },
-      p = { "<cmd>DiagnosticPrev<CR>", "jump 'Diagnostics' to previous" },
-      E = { "<cmd>DiagnosticLine<CR>", "show 'Diagnostics' diagnostic for current line" },
-    },
-  }, {
-    prefix = "<leader>",
+  wk.add({
+    ["<leader>x"] = { name = "Diagnostics" },
+    ["<leader>xL"] = { "<cmd>DiagnosticLine<CR>", "show 'Diagnostics' for current line" },
+    ["<leader>xn"] = { "<cmd>DiagnosticNext<CR>", "jump 'Diagnostics' to next" },
+    ["<leader>xp"] = { "<cmd>DiagnosticPrev<CR>", "jump 'Diagnostics' to previous" },
+    ["<leader>xE"] = { "<cmd>DiagnosticLine<CR>", "show 'Diagnostics' diagnostic for current line" },
   })
-  wk.register({
-    E = { "<cmd>DiagnosticLine<CR>", "show diagnostic for current line" },
+  wk.add({
+    ["E"] = { "<cmd>DiagnosticLine<CR>", "show diagnostic for current line" },
   })
 end
 
--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -2,19 +2,15 @@ local trouble = require("trouble")
 local wk = require("which-key")
 
 local function set_keymaps()
-  wk.register({
-    x = {
-      name = "Trouble",
-      x = { "<cmd>Trouble<cr>", "open / jump to" },
-      c = { "<cmd>TroubleClose<cr>", "close" },
-      w = { "<cmd>Trouble workspace_diagnostics<cr>", "workspace diagnostics" },
-      d = { "<cmd>Trouble document_diagnostics<cr>", "document_diagnostics" },
-      l = { "<cmd>Trouble loclist<cr>", "location list" },
-      q = { "<cmd>Trouble quickfix<cr>", "quickfix" },
-      -- r = { "<cmd>Trouble lsp_references<cr>", "LSP references" },
-    },
-  }, {
-    prefix = "<leader>",
+  wk.add({
+    ["<leader>x"] = { name = "Trouble" },
+    ["<leader>xx"] = { "<cmd>Trouble<cr>", "open / jump to" },
+    ["<leader>xc"] = { "<cmd>TroubleClose<cr>", "close" },
+    ["<leader>xw"] = { "<cmd>Trouble workspace_diagnostics<cr>", "workspace diagnostics" },
+    ["<leader>xd"] = { "<cmd>Trouble document_diagnostics<cr>", "document_diagnostics" },
+    ["<leader>xl"] = { "<cmd>Trouble loclist<cr>", "location list" },
+    ["<leader>xq"] = { "<cmd>Trouble quickfix<cr>", "quickfix" },
+    -- ["<leader>xr"] = { "<cmd>Trouble lsp_references<cr>", "LSP references" },
   })
 end
 
