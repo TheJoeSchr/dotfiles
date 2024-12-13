c9340db549d1baaa0d5744912f14859c32c1abd7
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-12-13 23:20:50 +0100

    fix: Resolve which-key mapping issues in Neovim config

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -2,15 +2,16 @@ local trouble = require("trouble")
 local wk = require("which-key")
 
 local function set_keymaps()
-  wk.add({
-    ["<leader>t"] = { group = "Trouble" },
-    ["<leader>tt"] = { "<cmd>Trouble<cr>", "open / jump to" },
-    ["<leader>tc"] = { "<cmd>TroubleClose<cr>", "close" },
-    ["<leader>tw"] = { "<cmd>Trouble workspace_diagnostics<cr>", "workspace diagnostics" },
-    ["<leader>td"] = { "<cmd>Trouble document_diagnostics<cr>", "document_diagnostics" },
-    ["<leader>tl"] = { "<cmd>Trouble loclist<cr>", "location list" },
-    ["<leader>tq"] = { "<cmd>Trouble quickfix<cr>", "quickfix" },
-    -- ["<leader>tr"] = { "<cmd>Trouble lsp_references<cr>", "LSP references" },
+  wk.register({
+    ["<leader>t"] = {
+      name = "Trouble",
+      t = { "<cmd>Trouble<cr>", "Open/Jump to" },
+      c = { "<cmd>TroubleClose<cr>", "Close" },
+      w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+      d = { "<cmd>Trouble document_diagnostics<cr>", "Document Diagnostics" },
+      l = { "<cmd>Trouble loclist<cr>", "Location List" },
+      q = { "<cmd>Trouble quickfix<cr>", "Quickfix" },
+    },
   })
 end
 
