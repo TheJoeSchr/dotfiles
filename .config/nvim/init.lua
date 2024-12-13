9957512a198ee085bc82ffaadc5994f473dada64
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-12-13 22:13:45 +0100

    refactor: Improve LSP configuration with comprehensive setup and integration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("plugins.firenvim")
--- require("config.plugins.lsp-format").setup()
--- require("config.plugins.neodev").setup()
--- require("config.plugins.lsp").setup()
--- require("config.plugins.null-ls").setup()
--- require("config.plugins.nvim-lsp-ts-utils").setup()
--- require("config.plugins.nvim-treesitter").setup()
--- require("config.plugins.nvim-treesitter-textobjects").setup()
+require("plugins.lsp")
