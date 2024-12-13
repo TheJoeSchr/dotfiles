caea37e84a68ddeb8f5a84269beea1afdf408313
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-12-13 23:03:12 +0100

    refactor: Change Trouble keymaps from <leader>x to <leader>t

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -3,14 +3,14 @@ local wk = require("which-key")
 
 local function set_keymaps()
   wk.add({
-    ["<leader>x"] = { name = "Trouble" },
-    ["<leader>xx"] = { "<cmd>Trouble<cr>", "open / jump to" },
-    ["<leader>xc"] = { "<cmd>TroubleClose<cr>", "close" },
-    ["<leader>xw"] = { "<cmd>Trouble workspace_diagnostics<cr>", "workspace diagnostics" },
-    ["<leader>xd"] = { "<cmd>Trouble document_diagnostics<cr>", "document_diagnostics" },
-    ["<leader>xl"] = { "<cmd>Trouble loclist<cr>", "location list" },
-    ["<leader>xq"] = { "<cmd>Trouble quickfix<cr>", "quickfix" },
-    -- ["<leader>xr"] = { "<cmd>Trouble lsp_references<cr>", "LSP references" },
+    ["<leader>t"] = { name = "Trouble" },
+    ["<leader>tt"] = { "<cmd>Trouble<cr>", "open / jump to" },
+    ["<leader>tc"] = { "<cmd>TroubleClose<cr>", "close" },
+    ["<leader>tw"] = { "<cmd>Trouble workspace_diagnostics<cr>", "workspace diagnostics" },
+    ["<leader>td"] = { "<cmd>Trouble document_diagnostics<cr>", "document_diagnostics" },
+    ["<leader>tl"] = { "<cmd>Trouble loclist<cr>", "location list" },
+    ["<leader>tq"] = { "<cmd>Trouble quickfix<cr>", "quickfix" },
+    -- ["<leader>tr"] = { "<cmd>Trouble lsp_references<cr>", "LSP references" },
   })
 end
 
