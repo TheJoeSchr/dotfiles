759afcf96e30bb758f6dcb84e131752fa7dbba81
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-12-14 18:05:05 +0100

    refactor: Remove unused import in keymaps configuration

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,3 @@
-local v = require("utils/maps") -- Options are automatically loaded before lazy.nvim startup
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
