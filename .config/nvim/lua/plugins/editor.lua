3d64b61707505858e71b01a943e014b45202ba75
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-12-15 20:21:15 +0100

    nvim: add coding.lua, set flash search on

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -97,4 +97,18 @@ return {
       { "<Leader>E", "<Plug>(easymotion-e)" },
     },
   },
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        -- options used when flash is activated through
+        -- a regular search with `/` or `?`
+        search = {
+          -- when `true`, flash will be activated during regular search by default.
+          -- You can always toggle when searching with `require("flash").toggle()`
+          enabled = true,
+        },
+      },
+    },
+  },
 }
