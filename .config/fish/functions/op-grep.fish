4f9c031c6c5d1ad3f7d3bfc5eb6ee419bea30e00
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2025-01-21 18:38:25 +0100

    fish: fix op-grep

--- .config/fish/functions/op-grep.fish
+++ .config/fish/functions/op-grep.fish
@@ -1,5 +1,6 @@
 # Defined in - @ line 1
 function op-grep --description alias\ op-pw-grep="op list items | grep \$1 | show field \$2"
-  set -q argv[2]; or set argv[2] "password"
-  op item list --format json  | jq ".[] | select (.title | contains (\"$argv[1]\")) | .id" | xargs -I % sh -c "op item get % --fields $argv[2]; echo \"\"";
+    set -q argv[2]; or set argv[2] password
+    op item list --format json | jq ".[] | select (.title | contains (\"$argv[1]\")) | .id" | xargs -I % sh -c "op item get % --reveal --fields $argv[2]; echo \"\""
+
 end
