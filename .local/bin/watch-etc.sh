642c61db2cf18c8f2275f406fa8c45e9aeb1e7c4
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2025-03-09 15:15:27 +0100

    watch-etc.sh: run du with sudo

--- .local/bin/watch-etc.sh
+++ .local/bin/watch-etc.sh
@@ -1,9 +1,8 @@
 #!/bin/env bash
-# original inotifywait -qq -r -e
 while sudo inotifywait -r -e close_write -e delete /etc; do
 
   (
     /usr/bin/echo -en "$(date '+%Y%m%d%H%M%S')\t"
-    du -sm | cut -f1
+    sudo du -sm | cut -f1
   ) >>~/du.log
 done
