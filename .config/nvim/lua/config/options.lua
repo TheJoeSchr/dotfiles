79a6bcfd27acfb7bb5cf197a151e3a6b1dbfbeef
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2025-04-13 18:53:19 +0200

    nvim: further fix clipboard issue using SSH_TTY

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -23,8 +23,10 @@ vim.cmd([[ let g:python3_host_prog = expand('/usr/bin/python') ]])
 vim.cmd([[au FocusLost * silent! wa]])
 
 local opt = vim.opt
--- clipboard
--- explicitly set clipboard
+
+-- Clipboard
+-- Explicitly set Clipboard
+-- Also fixes if SSH_TTY is set in tmux session by mobile app
 vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
 -- Predefine clipboard provider to speedup start
 vim.g.clipboard = {
@@ -39,6 +41,40 @@ vim.g.clipboard = {
   },
   cache_enabled = 1,
 }
+-- clipboard overrides is needed as sometimes SSH_TTY is set in tmux session by mobile app
+-- We need to customize the clipboard depending on whether in tmux, in SSH_TTY or not.
+--  In Tmux, there are 2 clipboard providers
+--  1. Tmux
+--  2. OSC52 should also work by default.
+--  In SSH_TTY, OSC 52 should work, but needs to be overridden as I use Alacritty.
+--  In local (not SSH session), LazyVim default clipboard providers can be used.
+--   Sample references -
+--   https://github.com/folke/which-key.nvim/issues/584 - Has references to when clipboard is modified
+--   Some more info here - https://www.reddit.com/r/neovim/comments/17rbbec/neovim_nightly_now_you_can_copy_via_ssh_with/
+--
+--
+--  You can test OSC 52 in terminal by using following in your terminal
+--  printf $'\e]52;c;%s\a' "$(base64 <<<'hello world')"
+local is_tmux_session = vim.env.TERM_PROGRAM == "tmux" -- Tmux is its own clipboard provider which directly works.
+-- TMUX documentation about its clipboard - https://github.com/tmux/tmux/wiki/Clipboard#the-clipboard
+if vim.env.SSH_TTY and not is_tmux_session then
+  local function paste()
+    return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+  end
+  local osc52 = require("vim.ui.clipboard.osc52")
+
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+end
 -- -- Show matching brackets.
 opt.showmatch = true
 -- -- Do smart case matching
