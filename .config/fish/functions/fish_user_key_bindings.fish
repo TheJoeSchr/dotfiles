9dbcdcfd93fc3d267ccc4536e910a6f0ff283ad6
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2025-04-29 14:18:09 +0200

    fish: format fish_user_key_bindings.fish

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,11 +1,11 @@
 function fish_user_key_bindings
-  if type -q "fileexplorer_user_key_bindings"
-    fileexplorer_user_key_bindings
-  end
-  if type -q "fzf_key_bindings"
-    fzf_key_bindings
-  end
-  if type -q "autocomplete_key_bindings"
-    autocomplete_key_bindings
-  end
+    if type -q fileexplorer_user_key_bindings
+        fileexplorer_user_key_bindings
+    end
+    if type -q fzf_key_bindings
+        fzf_key_bindings
+    end
+    if type -q autocomplete_key_bindings
+        autocomplete_key_bindings
+    end
 end
