00a7d074c1523a5c268644a70ac6b618c3c75bf7
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2025-07-19 14:12:07 +0200

    nvim: add autocmd to run terminal cmd and put output in buffer

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,28 @@
+-- // https://yobibyte.github.io/vim.html
+--
+-- a seamless integration with the terminal so that you could grep/find/build your project and direct the output to your editor without any hassle.
+--
+-- I have this seamless integration by having a simple custom binding that creates a scratch buffer, runs a command, and sends the output to that scratch buffer:
+
+
+--nread pipe input buffer
+vim.keymap.set("n", "<localleader>s", function()
+    vim.ui.input({}, function(c)
+        if c and c~="" then
+            vim.cmd("noswapfile vnew")
+          vim.bo.buftype = "nofile"
+          vim.bo.bufhidden = "wipe"
+          vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.fn.systemlist(c))
+        end
+    end)
+end)
+
+-- I can even get rid of this binding and just use ":vnew | read !cmd", but I do not want to manually close that buffer.
+
+
+
+
+
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
@@ -8,7 +33,7 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 
 -- Help Neovim check if file has changed on disk
 -- https://github.com/neovim/neovim/issues/2127
-local checktime_group = vim.api.nvim_create_augroup("checktime", { clear = true })
+local checktime_group = vim.api.nvim_create_augroup("cspciwciwllacehecktime", { clear = true })
 if vim.fn.has("gui_running") == 0 then
   vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "BufEnter", "FocusLost", "WinLeave" }, {
     group = checktime_group,
