599e9d14bbf2961f1c7caa476039231f0b4c4937
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2025-09-06 15:48:53 +0200

    filebot: try fixing lib refresh

--- .local/bin/filebot--move-finished.sh
+++ .local/bin/filebot--move-finished.sh
@@ -2,7 +2,7 @@
 
 rm /mnt/diskpool/NoMirror/Media/amc.txt
 
-filebot -rename -script fn:amc --log-file /mnt/diskpool/NoMirror/Media/amc.log --output "/mnt/diskpool/NoMirror/Media" --action move -non-strict "/mnt/diskpool/NoMirror/Downloads/Finished" --conflict auto --def excludeList=amc.txt movieFormat="{emby}" seriesFormat="{emby}" subtitles=en music=n emby=localhost/emby:HJ3ANN_TcGDkziNeLKza plex=127.0.0.1:LqvDPrUaDzsF_SsQMsaT deleteAfterExtract=n clean=y unsorted=y minFileSize=10
+filebot -rename -script fn:amc --log-file /mnt/diskpool/NoMirror/Media/amc.log --output "/mnt/diskpool/NoMirror/Media" --action move -non-strict "/mnt/diskpool/NoMirror/Downloads/Finished" --conflict auto --def excludeList=amc.txt movieFormat="{emby}" seriesFormat="{emby}" subtitles=en music=n emby=localhost:HJ3ANN_TcGDkziNeLKza plex=127.0.0.1:LqvDPrUaDzsF_SsQMsaT deleteAfterExtract=n clean=y unsorted=y minFileSize=10
 
 # 1eb949eb75be5c2e64884ab6eb2f1fc8c11c63ed
 # --def movieFormat="M:/{plex}" seriesFormat="S:/{plex}" animeFormat="T:/{plex}" musicFormat="D:/{plex}"
