485ae75c75243bac10c55ed30c5b94bca2946956
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2025-12-20 12:21:40 +0100

    rustdesk: add scripts to install relay server services and client installs

---
+++ Downloads/rustdeskclientinstall.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+# Assign a random value to the password variable
+rustdesk_pw=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+
+# Get your config string from your Web portal and Fill Below
+rustdesk_cfg="secure-string"
+
+################################### Please Do Not Edit Below This Line #########################################
+
+# Check if the script is being run as root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
+# Identify OS
+if [ -f /etc/os-release ]; then
+  # freedesktop.org and systemd
+  . /etc/os-release
+  OS=$NAME
+  VER=$VERSION_ID
+
+  UPSTREAM_ID=${ID_LIKE,,}
+
+  # Fallback to ID_LIKE if ID was not 'ubuntu' or 'debian'
+  if [ "${UPSTREAM_ID}" != "debian" ] && [ "${UPSTREAM_ID}" != "ubuntu" ]; then
+    UPSTREAM_ID="$(echo ${ID_LIKE,,} | sed s/\"//g | cut -d' ' -f1)"
+  fi
+
+elif type lsb_release >/dev/null 2>&1; then
+  # linuxbase.org
+  OS=$(lsb_release -si)
+  VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+  # For some versions of Debian/Ubuntu without lsb_release command
+  . /etc/lsb-release
+  OS=$DISTRIB_ID
+  VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+  # Older Debian, Ubuntu, etc.
+  OS=Debian
+  VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSE-release ]; then
+  # Older SuSE etc.
+  OS=SuSE
+  VER=$(cat /etc/SuSE-release)
+elif [ -f /etc/redhat-release ]; then
+  # Older Red Hat, CentOS, etc.
+  OS=RedHat
+  VER=$(cat /etc/redhat-release)
+else
+  # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+  OS=$(uname -s)
+  VER=$(uname -r)
+fi
+
+# Checks the latest version of RustDesk
+RDLATEST=$(curl https://api.github.com/repos/rustdesk/rustdesk/releases/latest -s | grep "tag_name" | awk -F'"' '{print $4}')
+
+# Install RustDesk
+
+echo "Installing RustDesk"
+if [ "${ID}" = "debian" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Debian" ] || [ "${UPSTREAM_ID}" = "ubuntu" ] || [ "${UPSTREAM_ID}" = "debian" ]; then
+  wget https://github.com/rustdesk/rustdesk/releases/download/${RDLATEST}/rustdesk-${RDLATEST}-x86_64.deb
+  apt-get install -fy ./rustdesk-${RDLATEST}-x86_64.deb >/dev/null
+elif [ "$OS" = "CentOS" ] || [ "$OS" = "RedHat" ] || [ "$OS" = "Fedora Linux" ] || [ "${UPSTREAM_ID}" = "rhel" ]; then
+  wget https://github.com/rustdesk/rustdesk/releases/download/${RDLATEST}/rustdesk-${RDLATEST}.x86_64.rpm
+  yum localinstall ./rustdesk-${RDLATEST}.x86_64.rpm -y >/dev/null
+elif [ "${UPSTREAM_ID}" = "suse" ]; then
+  wget https://github.com/rustdesk/rustdesk/releases/download/${RDLATEST}/rustdesk-${RDLATEST}.x86_64-suse.rpm
+  zypper -n install --allow-unsigned-rpm ./rustdesk-${RDLATEST}.x86_64-suse.rpm >/dev/null
+else
+  echo "Unsupported OS"
+  # here you could ask the user for permission to try and install anyway
+  # if they say yes, then do the install
+  # if they say no, exit the script
+  exit 1
+fi
+
+# Run the rustdesk command with --get-id and store the output in the rustdesk_id variable
+rustdesk_id=$(rustdesk --get-id)
+
+# Apply new password to RustDesk
+rustdesk --password $rustdesk_pw &>/dev/null
+
+rustdesk --config $rustdesk_cfg
+
+systemctl restart rustdesk
+
+echo "All done! Please double check the Network settings tab in RustDesk."
+echo ""
+echo "..............................................."
+# Check if the rustdesk_id is not empty
+if [ -n "$rustdesk_id" ]; then
+  echo "RustDesk ID: $rustdesk_id"
+else
+  echo "Failed to get RustDesk ID."
+fi
+
+# Echo the value of the password variable
+echo "Password: $rustdesk_pw"
+echo "..............................................."
