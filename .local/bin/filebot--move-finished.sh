a9776a28bdad80d64f2a1aefdcd56e350301e3f4
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2026-02-12 14:50:13 +0100

    filebot: emby and nordvpn fix

--- .local/bin/filebot--move-finished.sh
+++ .local/bin/filebot--move-finished.sh
@@ -2,7 +2,7 @@
 
 rm /mnt/diskpool/NoMirror/Media/amc.txt
 
-filebot -rename -script fn:amc --log-file /mnt/diskpool/NoMirror/Media/amc.log --output "/mnt/diskpool/NoMirror/Media" --action move -non-strict "/mnt/diskpool/NoMirror/Downloads/Finished" --conflict auto --def excludeList=amc.txt movieFormat="{emby}" seriesFormat="{emby}" subtitles=en music=n emby=localhost:HJ3ANN_TcGDkziNeLKza plex=127.0.0.1:LqvDPrUaDzsF_SsQMsaT deleteAfterExtract=n clean=y unsorted=y minFileSize=10
+filebot -rename -script fn:amc --log-file /mnt/diskpool/NoMirror/Media/amc.log --output "/mnt/diskpool/NoMirror/Media" --action move -non-strict "/mnt/diskpool/NoMirror/Downloads/Finished" --conflict auto --def excludeList=amc.txt movieFormat="{emby}" seriesFormat="{emby}" subtitles=en music=n --def emby=127.0.0.1:12e1a74d754c456794f3a148b643c2d3 deleteAfterExtract=n clean=y unsorted=y minFileSize=10
 
 # 1eb949eb75be5c2e64884ab6eb2f1fc8c11c63ed
 # --def movieFormat="M:/{plex}" seriesFormat="S:/{plex}" animeFormat="T:/{plex}" musicFormat="D:/{plex}"
