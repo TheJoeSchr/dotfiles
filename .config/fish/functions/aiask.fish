c00bb8767193f550a5bebe74e9929037b43d9a73
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2026-02-17 08:40:38 +0100

    aiask: add args to quickly glob relevant files

--- .config/fish/functions/aiask.fish
+++ .config/fish/functions/aiask.fish
@@ -1,5 +1,5 @@
-function aiask --wraps='aider --message' --description 'alias aiask aider --message'
-    pushd ~/.aider
-    aider --subtree-only --aiderignore ~/.aider/.aideringore --no-git --no-gitignore --no-add-gitignore-files --no-auto-commits --skip-sanity-check-repo --no-watch-files --no-auto-lint --no-analytics --no-check-update --model gemini/gemini-flash-lite-latest --message "/ask $argv"
-    popd
+function aiask --wraps='aider --message' --description='Ask a question to the AI assistant using aider' --argument-names='question' 'additional arguments'
+    # pushd ~/.aider
+    aider --subtree-only --aiderignore ~/.aider/.aideringore --no-git --no-gitignore --no-add-gitignore-files --no-auto-commits --skip-sanity-check-repo --no-auto-lint --no-show-model-warnings --no-analytics --no-check-update --message "/ask $argv[1]" --watch-files (fdgg $argv[2] $argv[3]) $argv[4..-1]
+    # popd
 end
