210857a63f7595edc5a4ca74f9a4b1cad2f0f403
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2026-02-21 09:26:52 +0100

    nvim: add tpope rails plugins

---
+++ .config/nvim/lua/plugins/rails.lua
@@ -0,0 +1,15 @@
+return {
+  -- Format happens in `conform.lua`, no need to config here
+  -- tpope goodness
+  -- e.g:
+  -- :BundleInstall,
+  -- :A Opens test file and vice versa
+  -- :Emodel
+  -- :Econtroller
+  -- :Eview
+  -- :ERspec
+  {
+    "tpope/vim-rails",
+    dependencies = { "tpope/vim-bundler", "tpope/vim-rake" },
+  },
+}
