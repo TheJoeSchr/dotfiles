49ae9d9a08cbd0c0bf574bf9e0f327385db3e69d
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2026-02-21 10:12:04 +0100

    fish: add mise source when available

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -53,6 +53,9 @@ if status --is-interactive
     if type -q rbenv
       source (rbenv init - fish | psub)
     end
+    if type -q mise
+      mise activate fish | source
+    end
     if type -q fnm
       fnm env --use-on-cd | source
     end
