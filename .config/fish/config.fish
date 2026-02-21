4ac40d6d4eaa9515e8e298c82599fdc41751da3c
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2026-02-21 09:29:55 +0100

    fish: cleanup main config, fdgg for aiask

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
+# =============================================================================
+# ENVIRONMENT VARIABLES
+# =============================================================================
 set -gx SHELL (which bash) # needs bash for nvim python
 # export environment variable VMUX_EDITOR, either vim or nvim
 set -gx VMUX_EDITOR nvim
@@ -13,7 +16,14 @@ set -gx BROWSER google-chrome-stable
 set -gx DELTA_FEATURES diff-so-fancy
 # always try to set DISPLAY
 # set -q DISPLAY; or set -gx DISPLAY ":0"
-# add user bin to path
+
+
+# =============================================================================
+# PATH SETUP
+# =============================================================================
+fish_add_path ~/.rvm/bin
+fish_add_path ~/.rbenv/bin
+fish_add_path ~/.fnm
 fish_add_path ~/.bun/bin
 fish_add_path ~/.config/composer/vendor/bin
 fish_add_path ~/.pub-cache/bin/
@@ -27,48 +37,73 @@ if [ ! -f /run/.containerenv ] && [ ! -f /.dockerenv ]
     fish_add_path ~/.local/podman/bin
 end
 
-if test -f "$HOME/.rvm/scripts/rvm"
-    fish_add_path ~/.rvm/bin
-end
-
-# INTERACTIVE
+# =============================================================================
+# INTERACTIVE SESSION
+# =============================================================================
 if status --is-interactive
     echo "Starting fish shell interactively..."
-    set HOSTNAME (uname --nodename)
+        set HOSTNAME (uname --nodename)
     if test (uname --nodename) = steamdeck
         set is_steam_host true
     else
         set is_steam_host false
     end
 
+    # 1. Native Tool Hooks (No Fisher plugins required)
+    if type -q rbenv
+      source (rbenv init - fish | psub)
+    end
+    if type -q fnm
+      fnm env --use-on-cd | source
+    end
+    if type -q zoxide
+        zoxide init fish | source
+        abbr cd z
+    end
+
+    if type -q pyenv
+        pyenv init - | source
+    end
+
+
+    # using own prompt now
+    # if type -q omf
+    #   omf theme yimmy
+    # end
+
+    # 2. Key Bindings & UI
     # CTRL-D should not close the terminal
     # bind ctrl-d delete-char
     fish_vi_key_bindings
     # usually automatically, but call to overwrite
-    if functions -q fish_user_key_bindings
-        fish_user_key_bindings
-    end
-    # Ctrl-f directory
-    # Ctrl-Alt-v environment variable
+    if functions -q fish_user_key_bindings; fish_user_key_bindings; end
+
     if functions -q fzf_configure_bindings
-        fzf_configure_bindings --directory=\cf --variables=\e\cv 2>/dev/null
+        # Ctrl-f directory
+        # Ctrl-Alt-v environment variable
+        fzf_configure_bindings --directory=\cf --variables=\e\cv
+        # Setting fd as the default source for fzf
+        set -gx FZF_DEFAULT_COMMAND 'fd --type f'
     end
 
-    # Setting fd as the default source for fzf
-    set -gx FZF_DEFAULT_COMMAND 'fd --type f'
-
+    # 3. Abbreviations & Aliases
     # need `:` for multiline comment
     : '
 THE THREE TYPES OF ALIAS
 ==============================
 
- - alias: 
+ - alias:
     Make a something functionally equivalent to something else. This holds true if you press return, chain things together, pipe, redirect, and such. In recent versions of fish, context aware tab completion will work here too.
 
- - abbr: 
+ - abbr:
     Type something and have it expanded into something else after pressing space or semicolon (but not return). The new hotness.
 
- - function: 
+    Abbreviations are stored in a variable named fish_user_abbreviations. This is automatically created as a universal variable the first time an abbreviation is created. If you want your abbreviations to be private to a particular fish session you can put the following in your *~/.config/fish/config.fish* file before you define your first abbrevation:
+    # set -g fish_user_abbreviations
+
+    You can create abbreviations directly on the command line and they will be saved automatically and made visible to other fish sessions if fish_user_abbreviations is a universal variable. If you keep the variable as universal, abbr --add statements in config.fish will do nothing but slow down startup slightly.
+
+ - function:
     Make something functionally equivalent to one or more something elses, and optionally permute data that are being worked on with (argv).Strictly speaking, alias is itself just an alias for a function that blindly accepts argv and tacks them to the end of its something else. But this pattern is so prolific that it got its own name, so we will consider it unique for today.
 '
     # no upgrade by default for steamdeck
@@ -77,9 +112,8 @@ THE THREE TYPES OF ALIAS
     else
         abbr upgrade 'VISUAL=nvim pikaurspeed -Su --devel --needed --noconfirm'
     end
-    # Abbreviations are stored in a variable named fish_user_abbreviations. This is automatically created as a universal variable the first time an abbreviation is created. If you want your abbreviations to be private to a particular fish session you can put the following in your *~/.config/fish/config.fish* file before you define your first abbrevation:
-    # You can create abbreviations directly on the command line and they will be saved automatically and made visible to other fish sessions if fish_user_abbreviations is a universal variable. If you keep the variable as universal, abbr --add statements in config.fish will do nothing but slow down startup slightly.
-    # set -g fish_user_abbreviations
+
+    abbr ai 'aider --read ~/.aider/AGENTS.md --no-gitignore --watch-files'
     abbr c config
     abbr ca 'config add '
     abbr ccc 'config ci'
@@ -96,6 +130,8 @@ THE THREE TYPES OF ALIAS
     abbr gl 'PAGER=/usr/bin/less git lg'
     abbr gla 'PAGER=/usr/bin/less git lga'
     abbr gs 'git st'
+    abbr gh "op plugin run -- gh"
+    abbr glab "op plugin run -- glab"
     abbr install 'pikaur -S --needed --noconfirm'
     abbr lal 'la -l'
     abbr l y # goes to cd
@@ -127,30 +163,20 @@ THE THREE TYPES OF ALIAS
     abbr vimdiff "$EDITOR -d"
     abbr vultr 'vultr-cli --config ~/vultr-cli.yaml'
     abbr wget wcurl
-    abbr ai 'aider --read ~/.aider/AGENTS.md --no-gitignore --watch-files'
+    abbr Y 'sudo -E fish -c "y"'
+    abbr yfp y-filepicker
 
     alias pbcopy 'xsel --clipboard --input'
     alias pbpaste 'xsel --clipboard --output'
     alias config 'git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
     alias df 'duf --hide-mp /var/lib/snapd/snap/\*'
     alias du 'dust -d 1'
-    # alias gh "op plugin run -- gh"
-    # alias glab "op plugin run -- glab"
-    alias lf y
-    alias mail cmdg
-    alias Y 'sudo -E fish -c "y"'
-    alias yfp y-filepicker
-    alias wget wcurl
+    alias mail aerc
 
-    if type -q omf
-        omf theme yimmy
-    end
-    # zoxide init
-    if type -q zoxide
-        zoxide init fish | source
-        abbr cd z
-    end
+    # 4. SSH Agent bridge (Fisher plugin)
+    if functions -q fish_ssh_agent; fish_ssh_agent; end
 
+    # 5. Tool Loaders (Mamba, etc)
     # >>> mamba initialize (LAZY LOADED) >>>
     if type -q micromamba
         set -gx MAMBA_EXE (command -v micromamba)
@@ -180,10 +206,6 @@ THE THREE TYPES OF ALIAS
     end
     # <<< mamba initialize <<<
 
-    if type -q pyenv
-        pyenv init - | source
-    end
-
     if type -q pgcli
         abbr pgcli 'VISUAL=nvim pgcli'
     end
@@ -198,7 +220,7 @@ THE THREE TYPES OF ALIAS
         alias ls 'exa -G --icons'
         alias lll 'ls -lT --level=1'
         alias llla 'ls -alT --level=1'
-        alias tree 'lll -a --level=3'
+        alias tree 'exa -G --icons -T --level=3'
     end
 
     if type -q tmux
@@ -224,9 +246,10 @@ THE THREE TYPES OF ALIAS
     # autostart ssh-add
     # fish_ssh_agent
 end # /(INTERACTIVE)
+# 6. Non-interactive Hooks
 
-# noninteractive:
+# always including noninteractive:
 if type -q direnv
-    # direnv hook fish | source
-    eval (direnv hook fish)
+    direnv hook fish | source
+    # eval (direnv hook fish)
 end
--- .config/fish/functions/fdgg.fish
+++ .config/fish/functions/fdgg.fish
@@ -1,3 +1,3 @@
 function fdgg --wraps='fd -i --glob' --description 'fd -i --glob with glob pattern built from arguments'
-    fd -i --glob "./**/$argv[1]$argv[2]" # $argv[3..-1]
+    fd -i --glob "./**/$argv[1]$argv[2]" $argv[3..-1]
 end
