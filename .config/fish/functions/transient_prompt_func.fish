8be8b35a9f3ab7cfc5e1fea93640abb43a64c062
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2026-02-24 07:39:51 +0100

    fish: fix transient prompt to add cwd and be cleaner

--- .config/fish/functions/transient_prompt_func.fish
+++ .config/fish/functions/transient_prompt_func.fish
@@ -4,12 +4,12 @@ function transient_prompt_func
     # too confusing:
     # - a) setting it switches status
     # - b) shell exit 0 is no error
-    set -l pc "="
+    set -l pc ""
     switch (id -u)
         case 0
-            set pc $pc'!❯'
+            set pc $pc"\$!"
         case '*'
-            set pc $pc'❯'
+            set pc $pc"\$"
+            echo -en "$(set_color blue)($(prompt_pwd))$pc$(set_color normal) "
     end
-    echo -en "$pc"
 end
