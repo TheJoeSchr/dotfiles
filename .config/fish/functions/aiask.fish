ed013f3ca3ab8d2a55e81b70e2261a6cf9fe0914
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2026-08-19 09:48:23 +0200

    fish: fix aiask.fish

--- .config/fish/functions/aiask.fish
+++ .config/fish/functions/aiask.fish
@@ -1,5 +1,3 @@
-function aiask --wraps='aider --message' --description='Ask a question to the AI assistant using aider' --argument-names='question' 'additional arguments'
-    # pushd ~/.aider
-    aider --subtree-only --aiderignore ~/.aider/.aideringore --no-git --no-gitignore --no-add-gitignore-files --no-auto-commits --skip-sanity-check-repo --no-auto-lint --no-show-model-warnings --no-analytics --no-check-update --message "/ask $argv[1]" --watch-files (fdgg $argv[2] $argv[3]) $argv[4..-1]
-    # popd
+function aiask --wraps='aider --message' --description='Ask a question to the AI assistant using aider' --argument-names question additional_arguments
+    aider --subtree-only --aiderignore ~/.aider/.aiderignore --no-git --no-gitignore --no-add-gitignore-files --no-auto-commits --skip-sanity-check-repo --no-auto-lint --no-show-model-warnings --no-analytics --no-check-update --message "/ask $argv[1]" --watch-files (fdgg $argv[2] $argv[3]) $argv[4..-1]
 end
